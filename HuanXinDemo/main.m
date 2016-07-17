dee7442f4c74a992ad2fa78c9143fe8b3c840542
1500979815 <qq.com>
2016-07-17 15:41:33 +0800

    Initial Commit

---
+++ HuanXinDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HuanXinDemo
+//
+//  Created by 申露露 on 16/7/17.
+//  Copyright © 2016年 申露露. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
